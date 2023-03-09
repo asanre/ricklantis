6283fa4e88f8335d1f6a8d8d73c152d43c5c97b8
asanre <aser04@gmail.com>
2023-03-09 14:36:55 +0100

    A01: Remove retrofit dependencies

--- app/src/main/java/io/asanre/app/core/data/DataModule.kt
+++ app/src/main/java/io/asanre/app/core/data/DataModule.kt
@@ -1,6 +1,5 @@
 package io.asanre.app.core.data
 
-import com.skydoves.retrofit.adapters.result.ResultCallAdapterFactory
 import io.ktor.client.*
 import io.ktor.client.engine.okhttp.*
 import io.ktor.client.plugins.*
@@ -8,32 +7,11 @@ import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.plugins.logging.*
 import io.ktor.client.plugins.resources.*
 import io.ktor.serialization.gson.*
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 private const val BASE_URL = "https://rickandmortyapi.com/api/"
 
 val coreDataModule = module {
-    single {
-        val client = OkHttpClient.Builder()
-            .addInterceptor(HttpLoggingInterceptor()
-                .apply {
-                    setLevel(
-                        HttpLoggingInterceptor.Level.BODY
-                    )
-                }).build()
-
-        Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .addCallAdapterFactory(ResultCallAdapterFactory.create())
-            .client(client)
-            .build()
-    }
-
     single {
         HttpClient(OkHttp) {
             install(Resources)
