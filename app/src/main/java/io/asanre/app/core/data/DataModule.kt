4da3968f89f95db6d1ec28790144a6a7693db9e9
asanre <aser04@gmail.com>
2023-03-09 16:24:54 +0100

    A01: Use okhttp logging interceptor

--- app/src/main/java/io/asanre/app/core/data/DataModule.kt
+++ app/src/main/java/io/asanre/app/core/data/DataModule.kt
@@ -4,9 +4,9 @@ import io.ktor.client.*
 import io.ktor.client.engine.okhttp.*
 import io.ktor.client.plugins.*
 import io.ktor.client.plugins.contentnegotiation.*
-import io.ktor.client.plugins.logging.*
 import io.ktor.client.plugins.resources.*
 import io.ktor.serialization.gson.*
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
 
 private const val BASE_URL = "https://rickandmortyapi.com/api/"
@@ -14,16 +14,19 @@ private const val BASE_URL = "https://rickandmortyapi.com/api/"
 val coreDataModule = module {
     single {
         HttpClient(OkHttp) {
-            install(Resources)
+            engine {
+                addInterceptor(HttpLoggingInterceptor().apply {
+                    setLevel(
+                        HttpLoggingInterceptor.Level.BODY
+                    )
+                })
+            }
 
             defaultRequest {
                 url(BASE_URL)
             }
 
-            install(Logging) {
-                level = LogLevel.BODY
-            }
-
+            install(Resources)
             install(ContentNegotiation) {
                 gson()
             }
