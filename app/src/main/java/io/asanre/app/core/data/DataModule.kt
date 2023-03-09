9d7cadd04db8a30a3f590d9cc1236b0c5c43500e
asanre <aser04@gmail.com>
2023-03-09 14:19:40 +0100

    A01: Add type safe routes

--- app/src/main/java/io/asanre/app/core/data/DataModule.kt
+++ app/src/main/java/io/asanre/app/core/data/DataModule.kt
@@ -6,6 +6,7 @@ import io.ktor.client.engine.okhttp.*
 import io.ktor.client.plugins.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.plugins.logging.*
+import io.ktor.client.plugins.resources.*
 import io.ktor.serialization.gson.*
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -13,7 +14,7 @@ import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-private const val BASE_URL = "https://rickandmortyapi.com/"
+private const val BASE_URL = "https://rickandmortyapi.com/api/"
 
 val coreDataModule = module {
     single {
@@ -35,6 +36,8 @@ val coreDataModule = module {
 
     single {
         HttpClient(OkHttp) {
+            install(Resources)
+
             defaultRequest {
                 url(BASE_URL)
             }
--- app/src/main/java/io/asanre/app/data/service/CharacterApiService.kt
+++ app/src/main/java/io/asanre/app/data/service/CharacterApiService.kt
@@ -4,22 +4,29 @@ import io.asanre.app.data.model.CharacterResult
 import io.asanre.app.data.model.CharactersResponse
 import io.ktor.client.*
 import io.ktor.client.call.*
+import io.ktor.client.plugins.resources.*
 import io.ktor.client.request.*
+import io.ktor.resources.*
 
-class CharacterApiService(
-    private val client: HttpClient
-) {
+class CharacterApiService(private val client: HttpClient) {
     suspend fun getCharacterById(id: Int): Result<CharacterResult> {
         return runCatching {
-            val response = client.get("api/character/$id")
+            val response = client.get(Characters.ById(id))
             response.body()
         }
     }
 
     suspend fun getAllCharacters(page: Int): Result<CharactersResponse> {
         return runCatching {
-            val response = client.get("api/character?page=$page")
+            val response = client.get(Characters(page))
             response.body()
         }
+    }
+
+    @Resource("/character")
+    private class Characters(val page: Int? = null) {
+
+        @Resource("{id}")
+        class ById(val id: Int, val parent: Characters = Characters())
     }
 }
