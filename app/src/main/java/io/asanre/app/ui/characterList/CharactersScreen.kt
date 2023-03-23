29140ff198f0af9c09aef1e6d3e0675950d21709
asanre <aser04@gmail.com>
2023-03-23 13:32:13 +0100

    Enabled default validation to throw when not 2xx responses

--- app/src/main/java/io/asanre/app/ui/characterList/CharactersScreen.kt
+++ app/src/main/java/io/asanre/app/ui/characterList/CharactersScreen.kt
@@ -1,12 +1,29 @@
 package io.asanre.app.ui.characterList
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.material.Card
+import androidx.compose.material.LocalContentColor
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -73,12 +90,12 @@ fun Character.toItem() = CharacterListItem(
 
 @Composable
 fun CharactersScreen(
-    viewmodel: CharacterListViewmodel = koinViewModel(),
     modifier: Modifier = Modifier,
+    viewmodel: CharacterListViewmodel = koinViewModel(),
     onError: () -> Unit = {},
     onItemClick: (CharacterListItem) -> Unit,
 ) {
-    LaunchedEffect(viewmodel) { viewmodel.getCharacters() }
+    LaunchedEffect(Unit) { viewmodel.getCharacters() }
     val state by viewmodel.state.collectAsState()
     if (state.error) {
         onError()
@@ -86,7 +103,10 @@ fun CharactersScreen(
     }
 
     if (state.showEmptyScreen)
-        EmptyView(modifier) { viewmodel.getCharacters() }
+        EmptyView(
+            modifier = modifier,
+            onClick = viewmodel::getCharacters
+        )
     else
         CharacterListContent(
             state = state,
